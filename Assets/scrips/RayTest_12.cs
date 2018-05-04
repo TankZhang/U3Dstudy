bb67316db06110948c464148c56ed93f2f09227b
TankZhang <13926855+TankZhang@users.noreply.github.com>
2018-05-04 21:37:43 +0800

    gogo

---
+++ Assets/scrips/RayTest_12.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest_12 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SendRay();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	private Ray ray;
+	private RaycastHit raycast;
+
+	void SendRay()
+	{
+		//按下鼠标左键发射射线，打中了就销毁
+		if(Input.GetMouseButtonDown(0))
+		{
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray, out raycast))
+			{
+				GameObject.Destroy(raycast.collider.gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/scrips/ToolsClass_11.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolsClass_11 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(Mathf.Max(5,10));
+		//插值运算
+		Debug.Log(Mathf.Lerp(0,10,0.2f));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			Debug.Log(Screen.width);
+			Debug.Log(Screen.height);
+		}
+		//Debug.Log("时间"+Time.time);
+		//Debug.Log(Time.deltaTime);
+		//Time.timeScale=0;
+
+		//差值0~10
+		num = Mathf.Lerp(num,10,Time.deltaTime);
+		Debug.Log(num);
+	}
+	private float num = 0;
+}
