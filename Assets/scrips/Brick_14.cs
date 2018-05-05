9630a0fa546d1e6d3f80e46721c061586c531e9f
TankZhang <13926855+TankZhang@users.noreply.github.com>
2018-05-05 10:30:37 +0800

    0505GOGO

---
+++ Assets/scrips/Brick_14.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick_14 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckUnderPlane();
+	}
+
+private void CheckUnderPlane()
+{
+	Transform m_trans = gameObject.GetComponent<Transform>();
+	if(m_trans.position.y<-5)
+	{
+		GameObject.Destroy(gameObject);
+	}
+}
+	private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.name.Contains("bullet"))
+        {
+            GameObject.Destroy(gameObject);
+			GameObject.Destroy(collider.gameObject,1.0f);
+        }
+    }
+}
---
+++ Assets/scrips/RayDemo_13.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayDemo_13 : MonoBehaviour
+{
+
+    private int width = 10;
+    private int heigt = 5;
+    public GameObject prefebBrick;
+    public GameObject bullet;
+    private Ray ray;
+    private RaycastHit raycast;
+    private Transform m_transform;
+    // Use this for initialization
+    void Start()
+    {
+        m_transform = gameObject.GetComponent<Transform>();
+        CreateWall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SendBullet();
+    }
+    void CreateWall()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < heigt; j++)
+            {
+                GameObject go = GameObject.Instantiate(prefebBrick, new Vector3(i - 5, j, 0), Quaternion.identity);
+				go.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f));
+            }
+        }
+    }
+
+    void SendBullet()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycast))
+            {
+                //实例化子弹，发射子弹
+                GameObject go = (GameObject)GameObject.Instantiate(bullet, m_transform.position, Quaternion.identity);
+                Vector3 dir = raycast.point - m_transform.position;
+
+				Debug.DrawRay(m_transform.position,dir,Color.green,1.0f);
+                go.GetComponent<Rigidbody>().AddForce(dir * 100);
+            }
+        }
+    }
+}
